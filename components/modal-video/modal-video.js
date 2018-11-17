ba4da512025985df5c9bbba060d1e9bed8b95abf
Brian Tofte-Schumacher <btschu@uw.edu>
2018-11-16 17:05:23 -0800

    MVP for provider resource filter

--- components/modal-video/modal-video.js
+++ components/modal-video/modal-video.js
@@ -1,27 +1,10 @@
 (function (Drupal, $) {
     $(document).ready(function () {
-        var modalHTML = '<div class="modal fade js-modal-video" tabindex="-1" role="dialog" aria-label="Video player" aria-hidden="true"><div class="modal-dialog modal-lg modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-body p-0"><button type="button" class="close p-0 border-0 text-primary" data-dismiss="modal" aria-label="Close"><i class="fas fa-times-circle" data-fa-transform="grow-16"></i></button><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item js-modal-video-iframe" src="" allowscriptaccess="always"></iframe></div></div></div></div></div>';
-        // append the modal html to body
-        $(modalHTML).appendTo('body');
-// Gets the video src from the data-src on each button
-        var $videoSrc;
-        $('.js-modal-video-btn').click(function () {
-            $videoSrc = $(this).data('src');
+        // stop playing the video when the modal closes by resetting the iframe src
+        $(".uwm-modal-video .modal").on('hidden.bs.modal', function (e) {
+            var $this = $(this);
+            var $iframe = $this.find('iframe');
+            $iframe.attr("src", $iframe.attr("src")); 
         });
-
-// when the modal is opened autoplay it  
-        $('.js-modal-video').on('shown.bs.modal', function (e) {
-// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-            $(".js-modal-video-iframe").attr('src', $videoSrc);
-        })
-
-
-// stop playing the youtube video when I close the modal
-        $('.js-modal-video').on('hide.bs.modal', function (e) {
-            // remove current video
-            $(".js-modal-video-iframe").attr('src', '');
-        })
-
-// document ready  
     });
 })(Drupal, jQuery);
