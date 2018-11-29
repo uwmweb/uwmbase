5bb90db36a367feb89fe700be75e7c8774cfaa45
Brian Tofte-Schumacher <btschu@uw.edu>
2018-11-29 12:35:03 -0800

    UWM-CMS: Finish first round of provider resources filter and media

--- components/provider-resource/provider-resource-view.js
+++ components/provider-resource/provider-resource-view.js
@@ -8,21 +8,37 @@
     Drupal.behaviors.uwmedProviderResourcesLinkGenerator = {
 
         attach: function (context, settings) {
-            function generateLink() {
+
+            // move the header to below the fitlers
+            $('.view-header').insertAfter('.view-filters')
+            
+            // update the query string when a user clicks a filter so we can maintain the selections if the user navigates to a resource
+            // and then pushes back to return to the filter
+            $('#views-exposed-form-uwm-provider-resources-uwm-provider-resources-block label').on('click', function(event) {
+                var input = $(event.target).parent().prev();
+                var querystring = generateQueryString(input[0]);
+                history.pushState(null, '', [location.pathname, querystring].join(''));
+            });
+
+            function generateQueryString(clicked = null) {
                 var checked = $('#views-exposed-form-uwm-provider-resources-uwm-provider-resources-block input[checked=checked]');
+                
                 var querystring = '?';
     
                 $.each(checked, function(index, element) {
-                    querystring += element.name + '=' + element.value; 
+                    if( clicked && element.name == clicked.name) {
+                        querystring += clicked.name + '=' + clicked.value; 
+                    } else {
+                        querystring += element.name + '=' + element.value; 
+                    }
+
                     if(checked[index+1]){
                         querystring += '&';
                     } else {
                         querystring += '#filter';
                     }
                 });
-
-                var url = [location.protocol, '//', location.host, location.pathname, querystring].join('');
-                return url
+                return querystring;
             }
 
 
@@ -48,11 +64,32 @@
             };
 
             $('.js-link-copy').on('click', function(e) {
-                var url = generateLink();
+                var querystring = generateQueryString();
+                var url = [location.origin, location.pathname, querystring].join('');
                 copyToClipboard(url);
                 $(e.target).text('Link copied!')
 
             });
+
+            // add view more collapsibles around result groups
+            var $groups = $('.views-view-grid');
+            $groups.each(function(index, element) {
+                var heading = element.previousElementSibling;
+                console.log(heading.innerText + ' has ' + $(element).children('.card-deck').length + ' rows.');
+
+                if($(element).children('.card-deck').length > 2) {
+                    var collapsedElements = $(element).children(':nth-child(n+3)');
+                    var collapseWrap = $.parseHTML('<div class="collapse" id="viewMoreCollapse' + heading.innerText + '"></div>');
+                    var collapseAfter = $.parseHTML('<p><a aria-controls="viewMoreCollapse' + heading.innerText + '" aria-expanded="false" class="view-more-link collapsed" data-target="#viewMoreCollapse' + heading.innerText + '" data-toggle="collapse" role="button">View More</a></p>');
+                    $(collapsedElements).wrapAll(collapseWrap);
+                    var collapseContainer = $(collapsedElements).parent();
+                    $(collapseAfter).insertAfter(collapseContainer);
+                }
+            });
+
+            // make sure the view more events are added to our dynamically created view more elements
+            Drupal.behaviors.toggleViewMoreText.attach();
         }
     }
 })(jQuery, Drupal);
+
--- dist/js/view-more.js
+++ dist/js/view-more.js
@@ -7,20 +7,25 @@
     Drupal.behaviors.toggleViewMoreText = {
         attach: function attach() {
 
+            // add angle down icons
+            $(".view-more-link[aria-expanded='false']").each(function (index, value) {
+                if ($(value).children('.fa-angle-down').length == 0) {
+                    $(value).append('<i class="fas fa-angle-down fa-fw"></i>');
+                }
+
+                if (!$(value).attr('tabindex')) {
+                    $(value).attr('tabindex', 0);
+                }
+            });
+
             $('.collapse').on('shown.bs.collapse', function () {
-                $(".view-more-link[aria-expanded='true']").text("View Less");
+                $(".view-more-link[aria-expanded='true']").text('View Less').append('<i class="fas fa-angle-up fa-fw"></i>');
             });
 
             $('.collapse').on('hidden.bs.collapse', function () {
-                $(".view-more-link[aria-expanded='false']").text("View More");
+                $(".view-more-link[aria-expanded='false']").text("View More").append('<i class="fas fa-angle-down fa-fw"></i>');
             });
         }
     };
-
-    // We pass the parameters of this anonymous function are the global variables
-    // that this script depend on. For example, if the above script requires
-    // jQuery, you should change (Drupal) to (Drupal, jQuery) in the line below
-    // and, in this file's first line of JS, change function (Drupal) to
-    // (Drupal, $)
 })(Drupal, jQuery);
 //# sourceMappingURL=view-more.js.map
