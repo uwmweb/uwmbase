e6c417c5b6556bf8032751f2926cc49066765da4
Brian Tofte-Schumacher <btschu@uw.edu>
2018-12-18 23:39:58 -0800

    UWM-CMS: Search updates

--- dist/js/search-locations.js
+++ dist/js/search-locations.js
@@ -1,68 +1,72 @@
+'use strict';
+
 /**
  * @file
  * Custom JavaScript for UW Medicine.
  */
 //
-// (function ($, Drupal) {
-//
-//   Drupal.behaviors.uwmSearchLayout = {
-//
-//     attach: function (context, settings) {
-//
-//       var searchResults = settings.uwm_locations_search_api_index || {};
-//       var markers = [];
-//
-//       var locationsMap = $(".search-results-map #map", context).gmap3({
-//         center: [47.609722, -122.333056],
-//         zoom: 12,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP,
-//         key: "AIzaSyAdY8R5lQPdDO_ffF9WADgbYVsYmpfv3Vw"
-//       });
-//
-//       $.each(searchResults, function (b) {
-//
-//         var obj = {
-//           position: [b["lat"], b["lon"]],
-//           label: b["title"],
-//           icon: "https//maps.google.com/mapfiles/marker_green.png",
-//           content: "<em>" + b["title"] + "</em>"
-//         };
-//
-//         let marker = locationsMap.marker(obj);
-//
-//         marker.on("click", function (marker, event) {
-//           marker.setIcon("http://maps.google.com/mapfiles/marker_orange.png");
-//           setTimeout(function () {
-//             marker.setIcon("http://maps.google.com/mapfiles/marker_green.png");
-//             console.log(marker);
-//           }, 200);
-//         });
-//
-//         markers.push(marker);
-//
-//       });
-//
-//       locationsMap.marker(markers);
-//
-//       locationsMap.cluster({
-//         size: 200,
-//         markers,
-//         cb: function (markers) {
-//           if (markers.length > 1) {
-//             // 1 marker stay unchanged (because cb returns nothing)
-//             if (markers.length < 20) {
-//               return {
-//                 content: '<div class="cluster cluster-1">' + markers.length + '</div>',
-//                 x: -26,
-//                 y: -26
-//               };
-//             }
-//           }
-//         }
-//       });
-//     }
-//   };
-//
-// })(jQuery, Drupal);
-"use strict";
+(function ($, Drupal) {
+
+    Drupal.behaviors.uwmSearchLayout = {
+
+        attach: function attach(context, settings) {
+            var $searchInputVal = $('#views-exposed-form-uwm-locations-search-page-1 input[name="s"]').attr('value');
+            if ($searchInputVal) {
+                $('.results-alphabetical-disclaimer').hide();
+            }
+            //
+            //       var searchResults = settings.uwm_locations_search_api_index || {};
+            //       var markers = [];
+            //
+            //       var locationsMap = $(".search-results-map #map", context).gmap3({
+            //         center: [47.609722, -122.333056],
+            //         zoom: 12,
+            //         mapTypeId: google.maps.MapTypeId.ROADMAP,
+            //         key: "AIzaSyAdY8R5lQPdDO_ffF9WADgbYVsYmpfv3Vw"
+            //       });
+            //
+            //       $.each(searchResults, function (b) {
+            //
+            //         var obj = {
+            //           position: [b["lat"], b["lon"]],
+            //           label: b["title"],
+            //           icon: "https//maps.google.com/mapfiles/marker_green.png",
+            //           content: "<em>" + b["title"] + "</em>"
+            //         };
+            //
+            //         let marker = locationsMap.marker(obj);
+            //
+            //         marker.on("click", function (marker, event) {
+            //           marker.setIcon("http://maps.google.com/mapfiles/marker_orange.png");
+            //           setTimeout(function () {
+            //             marker.setIcon("http://maps.google.com/mapfiles/marker_green.png");
+            //             console.log(marker);
+            //           }, 200);
+            //         });
+            //
+            //         markers.push(marker);
+            //
+            //       });
+            //
+            //       locationsMap.marker(markers);
+            //
+            //       locationsMap.cluster({
+            //         size: 200,
+            //         markers,
+            //         cb: function (markers) {
+            //           if (markers.length > 1) {
+            //             // 1 marker stay unchanged (because cb returns nothing)
+            //             if (markers.length < 20) {
+            //               return {
+            //                 content: '<div class="cluster cluster-1">' + markers.length + '</div>',
+            //                 x: -26,
+            //                 y: -26
+            //               };
+            //             }
+            //           }
+            //         }
+            //       });
+        }
+    };
+})(jQuery, Drupal);
 //# sourceMappingURL=search-locations.js.map
