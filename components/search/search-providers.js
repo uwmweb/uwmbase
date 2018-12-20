0f8dc9b379668a82129cdfad3bbe9e97e017154e
Brian Tofte-Schumacher <btschu@uw.edu>
2018-12-20 12:41:52 -0800

    Always show the alphabetical results

--- components/search/search-providers.js
+++ components/search/search-providers.js
@@ -5,13 +5,13 @@
 //
 (function ($, Drupal) {
 
-    Drupal.behaviors.uwmSearchLayout = {
+    // Drupal.behaviors.uwmSearchLayout = {
   
-      attach: function (context, settings) {
-          var $searchInputVal = $('#views-exposed-form-uwm-search-providers-page-1 input[name="s"]').attr('value');
-            if($searchInputVal) {
-                $('.results-alphabetical-disclaimer').hide();
-            }
-        }
-    }
+    //   attach: function (context, settings) {
+    //       var $searchInputVal = $('#views-exposed-form-uwm-search-providers-page-1 input[name="s"]').attr('value');
+    //         if($searchInputVal) {
+    //             $('.results-alphabetical-disclaimer').hide();
+    //         }
+    //     }
+    // }
 })(jQuery, Drupal);
--- dist/js/search-providers.js
+++ dist/js/search-providers.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @file
@@ -7,14 +7,14 @@
 //
 (function ($, Drupal) {
 
-    Drupal.behaviors.uwmSearchLayout = {
+  // Drupal.behaviors.uwmSearchLayout = {
 
-        attach: function attach(context, settings) {
-            var $searchInputVal = $('#views-exposed-form-uwm-search-providers-page-1 input[name="s"]').attr('value');
-            if ($searchInputVal) {
-                $('.results-alphabetical-disclaimer').hide();
-            }
-        }
-    };
+  //   attach: function (context, settings) {
+  //       var $searchInputVal = $('#views-exposed-form-uwm-search-providers-page-1 input[name="s"]').attr('value');
+  //         if($searchInputVal) {
+  //             $('.results-alphabetical-disclaimer').hide();
+  //         }
+  //     }
+  // }
 })(jQuery, Drupal);
 //# sourceMappingURL=search-providers.js.map
