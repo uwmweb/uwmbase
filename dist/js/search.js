f2c4b7a9a161196309ed91e9a72b620b38527445
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2018-12-26 13:04:54 -0800

    Features/search filters (#17)
    * UWM-BASE: Updating search filters to allow submit on enter-key press.
    
    * UWM-BASE: Adding filter guess for active item value. Allows re-running search with current filters.
    
    * UWM-BASE: Shortening trimmed result for provider and clinic searches.
    
    * UWM-BASE: Setting display if no search is entered.
    
    * UWM-BASE: Removing special characters; removing breakpoint call.
    
    * UWM-BASE: Restoring theme file.
    
    * UWM-BASE: Adding node link to search result.

--- dist/js/search.js
+++ dist/js/search.js
@@ -14,44 +14,24 @@
 
       var $resetButton = $("section.content-topper .submit-wrapper .btn-cta.reset");
 
-      var $selects = $(".selectpicker");
+      var $selects = $("section.content-topper .selectpicker");
 
       var clearSelections = function clearSelections() {
         $selects.selectpicker("deselectAll");
       };
+
       var showFilters = function showFilters() {
         $selects.parents(".dm-facets-selector").addClass("on");
       };
+
       var showFormControls = function showFormControls() {
         $(".views-exposed-form, .submit-wrapper").addClass("on");
-      };
-
-      var setActiveItems = function setActiveItems() {
-        //         $selects.each((j, k) => {
-        //           const $selectpicker = $(k);
-        //           const options = [];
-        //           $selectpicker.find("option").each((a, b) => {
-        //             const val = $(b).val();
-        //             const fVal = $(b).attr("data-facet-value");
-        //             const decodedUrl = decodeURI(window.location.search.replace(/%3A/g, ":"));
-        //             if (decodedUrl.indexOf(val) > 0) {
-        //               // $(b).attr("selected", "selected");
-        //               options.push(val);
-        //             } else if (
-        //               fVal.length > 0 &&
-        //               decodedUrl.indexOf(':' + fVal) > 0
-        //             ) {
-        //               options.push(val);
-        //             }
-        //           });
-        // //          $selectpicker.selectpicker("val", options);
-        //         });
       };
 
       $selects.selectpicker();
 
       $selects.on("loaded.bs.select", function () {
-        setActiveItems();
+        // console.log('selects loaded');
       });
 
       if (resultsCount > 0) {
@@ -67,12 +47,12 @@
   };
   Drupal.behaviors.initSearchForm = {
     attach: function attach(context, settings) {
-      var $searchForm = $("section.content-topper form.views-exposed-form");
-      var $searchInput = $searchForm.find("input[name=s]");
-      var $selectFilters = $("section.content-topper .selectpicker");
-      var inputVal = $searchInput.val();
-      var resultsCount = $("#main-container .views-view").data("view-total-rows");
 
+      var $searchForm = $("section.content-topper form.views-exposed-form");
+      var $selectFilters = $("section.content-topper .selectpicker");
+      var resultsCount = $("#main-container .views-view").data("view-total-rows");
+      var $searchInput = $searchForm.find("input[name=s]");
+      var inputVal = $searchInput.val();
       var $newSubmitButton = $("section.content-topper .submit-wrapper a.btn-cta.submit");
 
       var getSubmitUrl = function getSubmitUrl() {
@@ -112,10 +92,18 @@
         $newSubmitButton.trigger("click");
       });
 
-      $searchInput.keypress(function (event) {
-        if (event.which == 13) {
+      $searchInput.keypress(function (e) {
+        if (e.which == 13) {
           $newSubmitButton.trigger("click");
         }
+      });
+
+      $selectFilters.on("loaded.bs.select", function () {
+        $("section.content-topper button.btn.dropdown-toggle").keydown(function (e) {
+          if (e.which == 13) {
+            $newSubmitButton.trigger("click");
+          }
+        });
       });
     }
   };
